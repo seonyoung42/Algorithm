b3bca05dd7266dc84490b363070787be79729241
seonyoung <4tjsdud2@naver.com>
2021-08-21 22:14:38 +0900

    1966 풀이 (큐)

--- 백준/1966.playground/Contents.swift
+++ 백준/1966.playground/Contents.swift
@@ -1,89 +1,40 @@
 import Foundation
 
-
-let test = [ 1,4,5,7,2,8]
-test.enumerated()
-print(test[4])
-
 let firstLine = readLine() ?? ""
 let num = Int(firstLine) ?? 0
 
-
 for _ in 0..<num {
 
-    let secondLine = readLine() ?? ""
-    let thirdLine = readLine() ?? ""
-    let array = secondLine.components(separatedBy: " ")
-    let thirdArray = thirdLine.components(separatedBy: " ").enumerated()
+    let secondLine = readLine()!
+    let thirdLine = readLine()!
+    let secondArray = secondLine.components(separatedBy: " ")
+    var thirdArray = thirdLine.split(separator: " ").map{Int($0)!}
+    var indexArray : [(Int,Int)] = thirdArray.enumerated().map{($0,Int(String($1))!)}
     
     // enumerated : Array의 index 와 value 를 반환하는 메서드
+    // map : 클로저로 각 항목들을 반영한 결과물을 가진 배열 반환
+    // $0 : 첫번째 매개변수 -> 여기서는 index/ $1 : 두 번째 매개변수 -> 여기서는 값
     
     
-    let a = Int(array[0]) ?? 0
-    let b = Int(array[1]) ?? 0
-    var count = 1
+    let a = Int(secondArray[0]) ?? 0 // 문서 갯수
+    let b = Int(secondArray[1]) ?? 0 // 해당 문서의 인덱스
+    var count = 0
     
-    
-    
-    
-    for i in 0..<a {
+    while true {
+        let maxValue = thirdArray.max()
+        let maxIndex = thirdArray.index(of:maxValue!)
         
+        if indexArray[0].1 == thirdArray.max() {
+            count += 1
+            if indexArray[0].0 == b {
+                print(count)
+                break
+            } else {
+                indexArray.removeFirst()
+                thirdArray.remove(at: maxIndex!)
+            }
+        } else {
+            indexArray.append(indexArray.removeFirst())
+        }
     }
-    
-
-//    if a == 1 {
-//        print("1")
-//    } else {
-//        for i in 0..<a {
-//            if Int(thirdArray[i])! > Int(thirdArray[b])! {
-//                count += 1
-//            } else if Int(thirdArray[i])! == Int(thirdArray[b])! {
-//                same += 1
-//
-//            }
-//        }
-//    }
-    
-
-//    print(count)
 }
-
-
-
-
-
-
-
-
-
-//let thirdArray = [1,1,9,1,1,1]
-//let a = 6
-//let b = 0
-//var count = 1
-//var same = 0
-//
-//if a == 1 {
-//    print("1")
-//} else {
-//    for i in 0..<a {
-//        if Int(thirdArray[i]) > Int(thirdArray[b]) {
-//            count += 1
-//        } else if Int(thirdArray[i]) == Int(thirdArray[b]) {
-//            same += 1
-//
-//        }
-//    }
-//
-//    for i in 0..<a {
-//        for j in i+1..<a {
-//            if Int(thirdArray[i]) > Int(thirdArray[j]) {
-//
-//            }
-//        }
-//    }
-//
-//}
-//
-//
-//
-//print(count + same - (b+1) )
