c155da87f6c422708e5bac89d9e29e468b50fc44
seonyoung <4tjsdud2@naver.com>
2021-11-06 20:38:21 +0900

    9012 (괄호)

--- 백준/9012.playground/Contents.swift
+++ 백준/9012.playground/Contents.swift
@@ -3,9 +3,10 @@ import Foundation
 let testCase = Int(readLine()!)!
 
 for _ in 0..<testCase {
-    
+
     let line = readLine()!
     var count = 0
+
 
     for char in line {
         if char == "(" {
@@ -23,6 +24,8 @@ for _ in 0..<testCase {
 //    } else {
 //        print("NO")
 //    }
-    
+
+    // > 시간 크게 줄어드는 출력 방식
     print(count == 0 ? "YES" : "NO")
 }
+
