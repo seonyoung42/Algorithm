116b49ddd718011748a34a2ada9922cf94a26539
seonyoung <4tjsdud2@naver.com>
2021-11-17 16:21:57 +0900

    1676 (팩토리얼 0의 개수)

--- 백준/1676.playground/Contents.swift
+++ 백준/1676.playground/Contents.swift
@@ -1,45 +1,14 @@
 import Foundation
 
-let num = Int(readLine()!)!
+let n = Int(readLine()!)!
+var count = 0
 
-func factorial(n: Int) -> Int {
-    if n <= 1 {
-        return 1
-    } else {
-        return factorial(n: n-1) * n
-    }
-}
-
-func power(x:Double, n:Int) -> Double {
-    if n == 0 {
-        return 1
-    }
-    else if n > 0
-    {
-        if n % 2 == 0
-        {
-            return power(x: x, n: n / 2) * power(x: x, n: n / 2)
-        } else {
-            return x * power(x: x, n: n-1)
-        }
-    } else {
-        return 1 / power(x: x, n: -n)
-    }
-}
-
-
-func countZero(x:Int) -> Int {
-    
-    var count = 0
-    
-    while true {
-        
-        if x % Int(power(x: 10.0, n: count)) != 0 {
-            break
-        }
+for i in 1..<n+1 {
+    var num = i
+    while num>0, num%5 == 0 {
         count += 1
+        num /= 5
     }
-    
-    return count - 1
 }
-print(countZero(x: factorial(n: num)))
+
+print(count)
