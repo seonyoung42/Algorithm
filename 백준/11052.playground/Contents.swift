cd2940ae2585549b1182e4d8391231782e29dd62
seonyoung <4tjsdud2@naver.com>
2021-12-14 12:00:15 +0900

    11052 (카드 구매하기)

---
+++ 백준/11052.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let N = Int(readLine()!)!
+let costArray = readLine()!.split(separator: " ").map { Int(String($0))!}
+
+var dp = Array(repeating: 0, count: N+1)
+
+if costArray[0] == costArray.max() {
+    print(costArray[0]*N)
+} else {
+    dp[1] = costArray[0]
+    
+    for i in 2...N {
+        dp[i] = costArray[i-1]
+        let num = (i+1)/2
+        
+        for j in 1...num {
+            dp[i] = dp[i] > (dp[j] + dp[i-j]) ? dp[i] : (dp[j] + dp[i-j])
+        }
+    }
+    print(dp[N])
+}
+
