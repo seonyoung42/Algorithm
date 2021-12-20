20cb4190bd6afddfaf4a947a0f27935191736327
seonyoung <4tjsdud2@naver.com>
2021-12-20 12:24:17 +0900

    1149 (RGB 거리)

--- 백준/1149.playground/Contents.swift
+++ 백준/1149.playground/Contents.swift
@@ -1,25 +1,20 @@
 import Foundation
 
-
 let N = Int(readLine()!)!
-var nums = Array(repeating: Array(repeating: 0, count: 3), count: N+1)
+var nums : [[Int]] = [[]]
 
-for i in 0..<N {
+for _ in 0..<N {
     let a = readLine()!.split(separator: " ").map { Int(String($0))!}
-
-    for j in 0...2 {
-        nums[i][j] = a[j]
-    }
+    nums.append(a)
 }
 
 var dp = Array(repeating: Array(repeating: 0, count: 3), count: 1001)
-
 
 for i in 1...N {
     for j in 0...2 {
         let a = (j+1)%3
         let b = (j+2)%3
-        dp[i][j] = min(dp[i-1][a], dp[i-1][b]) + nums[i-1][j]
+        dp[i][j] = min(dp[i-1][a], dp[i-1][b]) + nums[i][j]
     }
 }
 
